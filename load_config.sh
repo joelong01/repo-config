bbb0e2a2f4e301404ae67197ef3504182c78fb6e
Joe Long <joelong@outlook.com>
2024-09-20 20:33:24 +0000

    integrating with .zshrc so starting a terminal loads the config into the environment

---
+++ load_config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# this file is where you run the commands to load the configurations in your project.
+# there can be many configurations, depending on how the app is structured.  in this case, we are
+# just loading the test configurations.
+# we use --silent because after the initial collection of data, we do not want to be bothered
+# everytime a terminal is opened
+
+# NOTE:  the executable repo_config must be part of the repo for this to work.  in this project
+#        we just build it.  in any other project it should be installed through the docker file
+
+if [[ ! -f "repo-config" ]]; then
+    go build -o repo-config
+fi
+
+output=$(./repo-config collect --json ./end-to-end-test/test-config.json --silent)
+
+result=$(echo $output | jq -r .status)
+
+if [[ "$result" != "ok" ]]; then
+    echo -n "ERROR: "
+    echo $result | jq .message
+fi
---
+++ load_env.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Add this to your .zshrc
+
+# Function to source .ENV files from the corresponding PROJECT_DIR
+source_env_files() {
+    # Get the current directory name (PROJECT_DIR)
+    PROJECT_DIR=$(basename "$PWD")
+    # Define the path in $HOME/.repo-config where the .ENV files should be located
+    CONFIG_DIR="$HOME/.repo-config/$PROJECT_DIR"
+    # Check if the directory exists
+    if [[ -d "$CONFIG_DIR" ]]; then
+        # Loop through all files ending in .env and source them
+        for env_file in "$CONFIG_DIR"/.*.env; do
+            # Check if any .ENV files are found
+            if [[ -f "$env_file" ]]; then
+                echo "sourcing $env_file"
+                # Source the .ENV file
+                source "$env_file"
+            fi
+        done
+    fi
+}
+
+# Call the function when the shell starts
+source_env_files
